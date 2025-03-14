3fd8937cc265cfe2e4b02230da741f117239c512
Manoj-6789 <mroul914@gmail.com>
2025-03-14 11:48:47 +0530

    Update DemoApplication.java

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
-
+      double d=20.3;
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
